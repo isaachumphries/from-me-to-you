2041affcf093d06d5274be26580b850e5d5fb42f
vamsim17 <vamsimarrapu17@gmail.com>
2026-03-21 15:42:41 -0500

    pulled from main and linked login button to login page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const loginBtn = document.getElementById("yesBtn");
+const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", () => {
     window.location.href = "index.html";
