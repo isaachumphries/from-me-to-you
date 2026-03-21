4b78645f6a16f2302f292f5aa8a765bc3f5db80c
vamsim17 <vamsimarrapu17@gmail.com>
2026-03-21 18:13:21 -0500

    changes to layout, appearance and button works

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const loginBtn = document.getElementById("loginBtn");
+const toMeBtn = document.getElementById("to_me");
+const fromYouBtn = document.getElementById("from_you");
 
 loginBtn.addEventListener("click", () => {
     window.location.href = "index.html";
+});
+toMeBtn.addEventListener("click", () => {
+    window.location.href = "social.html";
+});
+fromYouBtn.addEventListener("click", () => {
+    window.location.href = "personal.html";
 });
